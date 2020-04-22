f5658b8a6b9065976bda22d2eb7ad964c7891032
fiqhia02 <fiqhifiqhia@gmail.com>
2020-04-22 18:47:57 +0800

    second commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+route::get('/index','PetugasController@index') ->name('index');
+route::get('/Petugas/create','PetugasController@create') ->name('Petugas.create');
+route::post('/Petugas/store','PetugasController@store') ->name('Petugas.store');
+route::put('/Petugas/update/{Id_petugas}','PetugasController@update') ->name('Petugas.update');
+route::get('/Petugas/edit/{Id_petugas}','PetugasController@edit') ->name('Petugas.edit');
+route::get('/Petugas/destroy/{Id_petugas}','PetugasController@destroy') ->name('Petugas.destroy');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
